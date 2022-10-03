0dea8cced555049b77030c4aab235a04ccd3b696
Siamkhab <113988626+Siamkhab@users.noreply.github.com>
2022-10-03 15:10:13 +0600

    Create Siamkhan.py
    apt update
    apt upgrade
    pkg install python
    pkg install python2
    pkg install git
    pip2 install requests
    pip2 install mechanize
    git clone https://github.com/Siamkhab/siamk.git
    ls
    cd siamkhan.py
    ls
    python2 siamkhan.py

---
+++ Siamkhan.py
@@ -0,0 +1,22 @@
+# Tariful-Mail-2G
+
+
+
+🌋 apt update
+
+🌋 apt upgrade
+
+🌋 pkg install python
+
+🌋 pkg install python2
+
+🌋 pkg install git 
+
+🌋 pip2 install requests
+
+🌋 pip2 install mechanize
+
+🌋 git clone https://www.facebook.com/mdsiam.islam.31149359
+🌋 cd Talha-Mail-2G
+
+🌋 python ➳TARIFUL—͟͟͞͞𖣘Mail.py
